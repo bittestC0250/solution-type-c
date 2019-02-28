db9945d6df370b0d9f08e7c25f2b5f0c624d54a6
JHeeJung <sjj19950217@gmail.com>
2019-02-28 13:29:23 +0900

    solved

--- src/main/java/problem03/MyStack.java
+++ src/main/java/problem03/MyStack.java
@@ -4,20 +4,27 @@ public class MyStack {
 	
 	private String[] buffer;
 	public static int i=0;
+	public static int check=0;
 
 	public MyStack( int size ) {
+		check=size;
 		buffer= new String[size];
 		i=0;
 	}
 	
 	public void push(String item) {
+		if(i>=check) {
+			size();
+		}
 		buffer[i]=item;
 		i++;
 	}
 
 	public String pop() {
-		String value=buffer[i];
-		i--;
+		String value="";
+		if(i<check) {
+		value=buffer[i];
+		i--;}
 		return value;
 	}
 
@@ -29,6 +36,14 @@ public class MyStack {
 	}
 	
 	public int size() {
-		return 0;
+		String[] resize=new String[check+1];
+		
+		for(int k=0; k<buffer.length; k++) {
+			resize[k]=buffer[k];
+		}
+		buffer=new String[check+1];
+		buffer=resize;
+		
+		return check=check+1;
 	}
 }
